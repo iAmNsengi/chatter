e891b1dcb96c80a70b997d4bd7ddc2b24e98a5a1
Eliezer Nsengi <nsengitech@gmail.com>
2024-11-24 21:35:26 +0200

    feat: profile relover mutations

--- backend/src/profile/profile.resolver.ts
+++ backend/src/profile/profile.resolver.ts
@@ -1,4 +1,19 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver, Query } from '@nestjs/graphql';
+import { Profile } from './profile.type';
+import { CreateProfileDTO } from './dto/profile.dto';
+import { ProfileService } from './profile.service';
 
 @Resolver()
-export class ProfileResolver {}
+export class ProfileResolver {
+  constructor(private readonly profileService: ProfileService) {}
+
+  @Mutation(() => Profile)
+  async createProfile(@Args('input') input: CreateProfileDTO) {
+    return this.profileService.createProfile(input);
+  }
+
+  @Query(() => Profile)
+  async getProfileById(@Args('profileId') profileId: number) {
+    return this.profileService.getProfileById(profileId);
+  }
+}
