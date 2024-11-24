21d2fb8834b0c2393582ae15a4a588faf87cf8fd
Eliezer Nsengi <nsengitech@gmail.com>
2024-11-24 11:01:00 +0200

    add: server resolver query

--- backend/src/server/server.resolver.ts
+++ backend/src/server/server.resolver.ts
@@ -1,4 +1,10 @@
+import { Query } from '@nestjs/graphql';
 import { Resolver } from '@nestjs/graphql';
 
 @Resolver()
-export class ServerResolver {}
+export class ServerResolver {
+  @Query(() => String)
+  async hello(): Promise<string> {
+    return 'hello world';
+  }
+}
