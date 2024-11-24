730b47d9d1296d5f5c252a3e6d463de8bdcfe84d
Eliezer Nsengi <nsengitech@gmail.com>
2024-11-24 23:29:21 +0200

    fix: profile types

--- backend/src/profile/profile.resolver.ts
+++ backend/src/profile/profile.resolver.ts
@@ -2,11 +2,14 @@ import { Args, Mutation, Resolver, Query } from '@nestjs/graphql';
 import { Profile } from './profile.type';
 import { CreateProfileDTO } from './dto/profile.dto';
 import { ProfileService } from './profile.service';
+import { UseGuards } from '@nestjs/common';
+import { GraphlAuthGuard } from 'src/auth/auth.guard';
 
 @Resolver()
 export class ProfileResolver {
   constructor(private readonly profileService: ProfileService) {}
 
+  @UseGuards(GraphlAuthGuard)
   @Mutation(() => Profile)
   async createProfile(@Args('input') input: CreateProfileDTO) {
     return this.profileService.createProfile(input);
