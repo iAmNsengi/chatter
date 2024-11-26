719e5546ec3b934a480e27be8af98fa20781dcd7
Eliezer Nsengi <nsengitech@gmail.com>
2024-11-26 15:54:01 +0200

    create: createserver dto

--- backend/src/server/server.service.ts
+++ backend/src/server/server.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
-export class ServerService {}
+export class ServerService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async createServer(input: CreateServerDTO);
+}
