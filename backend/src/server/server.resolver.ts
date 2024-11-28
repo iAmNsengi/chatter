7cdc99db3d7283b9631c1059c676b1abbc3ee0f8
Eliezer Nsengi <nsengitech@gmail.com>
2024-11-28 08:29:47 +0200

    fix: getservers

--- backend/src/server/server.resolver.ts
+++ backend/src/server/server.resolver.ts
@@ -1,10 +1,19 @@
-import { Query } from '@nestjs/graphql';
+import { Args, Context, Query } from '@nestjs/graphql';
 import { Resolver } from '@nestjs/graphql';
+import { Server } from './types';
+import { UseGuards } from '@nestjs/common';
+import { GraphlAuthGuard } from 'src/auth/auth.guard';
+import { ApolloError } from 'apollo-server-express';
 
 @Resolver()
 export class ServerResolver {
-  @Query(() => String)
-  async hello(): Promise<string> {
-    return 'hello world';
+  @UseGuards(GraphlAuthGuard)
+  @Query(() => [Server])
+  async getServers(
+    @Args('profileId') profileId: number,
+    @Context() ctx: { req: Request },
+  ) {
+    if (!ctx.req.profile.email)
+      return new ApolloError('Profile not found', 'PROFILE_NOT_FOUND');
   }
 }
